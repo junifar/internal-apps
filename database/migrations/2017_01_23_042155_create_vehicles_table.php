da641b2144e164e522baf36273e18361dba1ac01
junifar <junifar@gmail.com>
2017-01-23 11:25:29 +0700

    adding vehicle

---
+++ database/migrations/2017_01_23_042155_create_vehicles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('plat_number')->unique();
+            $table->string('name')->nullable();
+            $table->string('no_stnk')->nullable();
+            $table->integer('driver_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
